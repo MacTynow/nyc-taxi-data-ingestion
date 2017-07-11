6566deee364560305d079ec0c4f8459e8043aa90
Charles Martinot <charles.martinot@honestbee.com>
2017-07-11 17:50:00 +0800

    Fix missing kafka config

--- ingest.py
+++ ingest.py
@@ -8,6 +8,10 @@ sample_file = "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2016-0
 kafka_brokers = os.getenv("KAFKA_BROKERS", "localhost:9092")
 kafka_topic = os.getenv("KAFKA_TOPIC", "test")
 
+client = KafkaClient(hosts=kafka_brokers)
+topic = client.topics[kafka_topic.encode("utf8")]
+producer = topic.get_producer()
+
 if not os.path.isfile(sample_file):
   url = sample_file
   response = requests.get(url, stream=True)
