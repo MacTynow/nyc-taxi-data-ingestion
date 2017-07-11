eec1e2fceb436108900114533cfe5ab7c5e4523c
Charles Martinot <charles.martinot@honestbee.com>
2017-07-11 17:05:48 +0800

    Pushing data as json

--- ingest.py
+++ ingest.py
@@ -1,21 +1,25 @@
 import csv
 import os
 import requests
+import json
 from pykafka import KafkaClient
 
 sample_file = "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2016-01.csv"
-kafka_brokers = os.getenv("KAFKA_BROKERS", "localhost:9092")
-kafka_topic = os.getenv("KAFKA_TOPIC", "test")
-
-
-client = KafkaClient(hosts=kafka_brokers)
-topic = client.topics[kafka_topic.encode("utf8")]
-producer = topic.get_producer()
-
+# kafka_brokers = os.getenv("KAFKA_BROKERS", "localhost:9092")
+# kafka_topic = os.getenv("KAFKA_TOPIC", "test")
 
 if not os.path.isfile(sample_file):
   url = sample_file
   response = requests.get(url, stream=True)
 
-  for data in response.iter_lines():
-    producer.produce(data)
+  for line in response.iter_lines():
+    data = {}
+    data["pickup_time"] = line.split(",")[1]
+    data["dropoff_time"] = line.split(",")[2]
+    data["pickup_long"] = line.split(",")[5]
+    data["pickup_lat"] = line.split(",")[6]
+    data["dropoff_long"] = line.split(",")[9]
+    data["dropoff_lat"] = line.split(",")[10]
+    json_data = json.dumps(data)
+    print json_data
+    # producer.produce(json_data)
